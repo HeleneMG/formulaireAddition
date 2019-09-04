69b0acb1a8f5038bd8593a7f5b0e66a07b0f0cff
HeleneMG <helenemouttetg@gmail.com>
2019-09-04 09:37:49 +0200

    premier commit

---
+++ addition.js
@@ -0,0 +1,12 @@
+function addition() {
+    let nb1 = parseInt(document.getElementById("nb1").value);
+    let nb2 = parseInt(document.getElementById("nb2").value);
+    let result = document.getElementById("result");
+    result.value = nb1 + nb2;
+}
+function soustraction() {
+    let nb1 = parseInt(document.getElementById("nb1").value);
+    let nb2 = parseInt(document.getElementById("nb2").value);
+    let result = document.getElementById("result");
+    result.value = nb1 - nb2;
+}
